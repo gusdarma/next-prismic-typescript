507539e82088d97a76be8dbb28b5b4bf932767f4
gusdarma <gusdarmap@gmail.com>
2021-11-17 17:57:03 +0800

    add slice-machine and connect next with prismic

---
+++ .slicemachine/assets/slices/Hero/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Hero';
+
+export default {
+  title: 'slices/Hero'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"hero","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent frictionless technologies","spans":[]}],"description":[{"type":"paragraph","text":"Veniam cupidatat deserunt aliqua non fugiat occaecat reprehenderit cupidatat consectetur nostrud exercitation nulla magna laborum nisi. Anim nisi dolore adipisicing ea labore ullamco veniam. Enim ex ad aliqua laborum aliqua fugiat.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
